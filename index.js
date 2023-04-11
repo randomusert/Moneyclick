a377daa931c0e95a15f8c0d1f3f59d2ecd74d013
randomusert <127417963+randomusert@users.noreply.github.com>
2023-04-11 18:59:48 +0300

    added upgrades and started to do save

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+//variables
+//sets variable called score to zero
 var score = 0;
+var cursorCost = 2;
+var cursors = 0;
+
+function BuyCursors() {
+    if(score >= cursorCost) {
+        score = score - cursorCost;
+        cursors = cursors + 1;
+        cursorCost = Math.round(cursorCost * 5);
+        document.getElementById("cursors").innerHTML = cursors;
+        document.getElementById("score").innerHTML = score;
+        document.getElementById("cursorCost").innerHTML = cursorCost;
+    }
+}
 
 function getmoney() {
-    score = score + 1;
+    score = score + 5;
     document.getElementById("score").innerHTML = score;
 }
+
+setInterval(function() {
+    score = score + cursors *5;
+    document.getElementById("score").innerHTML = score;
+}, 1000)
+function Save(){}
