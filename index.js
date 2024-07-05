47c23344d89c22af2d042118791d3e5db49adffa
randomusert <127417963+randomusert@users.noreply.github.com>
2024-07-05 20:37:01 +0300

    save and load system

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 var score = 0;
 var cursorCost = 2;
 var cursors = 0;
+//load the save on the website loading
+window.onload = function() {
+    Load()
+}
 
 function BuyCursors() {
     if(score >= cursorCost) {
@@ -15,14 +19,36 @@ function BuyCursors() {
     }
 }
 
+
 function getmoney() {
     score = score + 5;
     document.getElementById("score").innerHTML = score;
 }
 
+
 setInterval(function() {
     score = score + cursors *5;
     document.getElementById("score").innerHTML = score;
 }, 1000)
+
+
+
 //the save function
-function Save(){}
+function Save(){
+    var gamesave = {
+        score: score,
+        cursorCost: cursorCost,
+        cursors: cursors
+    }
+    localStorage.setItem("gamesave", JSON.stringify(gamesave))
+}
+function Load() {
+    var SavedGame = JSON.parse(localStorage.getItem("gamesave"))
+
+    if (typeof SavedGame.score !== "undefined") score = SavedGame.score
+    if (typeof SavedGame.cursorCost !== "undefined") cursorCost = SavedGame.cursorCost
+    if (typeof SavedGame.cursors !== "undefined") cursors = SavedGame.cursors
+}
+setInterval(function() {
+    Save()
+},30000)
