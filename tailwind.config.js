22a98bdf7402b2687d66fd45283f0f2439464e52
randomusert <127417963+randomusert@users.noreply.github.com>
2024-10-18 05:47:15 +0000

    part 2 of 0.6.3

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'money-green':'#488c1d'
+      }
+    },
   },
   plugins: [],
 }
